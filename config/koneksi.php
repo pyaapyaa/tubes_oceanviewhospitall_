0a6941ac0eed9ad3bd9ad76faa51411c00fb57b1
Nalfaww <nalfaw.we@gmail.com>
2026-07-08 00:45:11 +0800

    Update koneksi database untuk InfinityFree

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "oceanviewhospital";
+$host = "sql106.infinityfree.com";
+$user = "if0_42357148";
+$pass = "1VaffiN9wQJk";
+$db   = "if0_42357148_oceanviewhospital";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
